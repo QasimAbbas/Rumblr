8c2bd2573b673360003be3ce480235007f0d5d0f
Roopeswar Kommalapati <roopeshkom@gmail.com>
2016-08-21 16:37:51 -0400

    Changed models again, for his animations. Added movement, now adding attacks

---
+++ _Scripts/PlayerMovementController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	private static Animator anim;
+	private static float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = Input.GetAxis ("Vertical");
+		anim.SetFloat ("speed", speed);
+	}
+}
