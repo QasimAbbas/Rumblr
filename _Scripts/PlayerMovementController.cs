e746f48686ecf89fbf63dd9478f821f599ad7c76
Roopeswar Kommalapati <roopeshkom@gmail.com>
2016-08-25 23:57:16 -0400

    Added blocking animation, sword state and collider control, and implemented jump checking

--- _Scripts/PlayerMovementController.cs
+++ _Scripts/PlayerMovementController.cs
@@ -3,26 +3,31 @@ using System.Collections;
 
 public class PlayerMovementController : MonoBehaviour {
 
-	public float moveSpeed, rotateSpeed, jumpSpeed;
+	private float moveSpeed=50f, rotateSpeed=5f, jumpSpeed=20f;
+	private static bool jumping, attacking;
 
-	private static Animator anim;
 	private static Rigidbody rb;
+	private static Animator anim;
 
 	// Use this for initialization
-	void Start () {
+	private void Start () {
+		jumping = false;
+
 		anim = GetComponent<Animator> ();
 		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		
 		//Controls movement animation
 		anim.SetFloat ("speed", Input.GetAxis ("Vertical"));
 
-		//Controls attacking
+		//Controls attacking, if the sword is active, and the attacking animations
 		if (Input.GetMouseButton (0)) {
 			anim.SetBool ("attack", true);
+			StopAllCoroutines ();
+			StartCoroutine (SetSwordActive ());
 		} else {
 			anim.SetBool ("attack", false);
 		}
@@ -31,9 +36,28 @@ public class PlayerMovementController : MonoBehaviour {
 		rb.AddForce (Input.GetAxisRaw("Vertical") * transform.forward * moveSpeed);
 		transform.RotateAround (transform.position, Vector3.up, Input.GetAxis ("Horizontal") * rotateSpeed);
 
-		//Controls jumping
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		//Controls jumping, if I am not already doing so
+		if (Input.GetKeyDown (KeyCode.Space) && !jumping) {
+			jumping = true;
 			rb.AddForce (Vector3.up * jumpSpeed, ForceMode.Impulse);
+		}
+	}
+
+	//Sets the sword object active for length of current attacking animation being played
+	private IEnumerator SetSwordActive(){
+		GetComponentInChildren<SphereCollider> ().enabled = true;
+		GetComponentInChildren<TrailRenderer> ().enabled = true;
+
+		yield return new WaitForSeconds (1f);
+
+		GetComponentInChildren<SphereCollider> ().enabled = false;;
+		GetComponentInChildren<TrailRenderer> ().enabled = false;
+	}
+
+	private void OnCollisionEnter(Collision other){
+		//Checks if I touched down, and allows me to jump again
+		if (other.collider.CompareTag ("ground")) {
+			jumping = false;
 		}
 	}
 }
